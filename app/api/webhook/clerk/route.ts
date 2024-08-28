3170902293a753875e1e36a569d160138327c3e0
hrvoje vincek <vincekhrvoje@gmail.com>
2024-08-28 21:40:11 +0200

    changes

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -52,11 +52,7 @@ export async function POST(req: Request) {
   }
 
   // Get the ID and type
-  const { id } = evt.data;
   const eventType = evt.type;
-
-  console.log(eventType);
-  console.log(evt.data);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
