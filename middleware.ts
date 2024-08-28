0ce36334738d2ab0aae39a1c98549075fef81d9c
hrvoje vincek <vincekhrvoje@gmail.com>
2024-08-28 21:10:05 +0200

    middelware update

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/api/webhook/clerk",
-  "/api/webhook/stripe",
-  "/api/uploadthing",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware(() => {});
 
 export const config = {
   matcher: [
